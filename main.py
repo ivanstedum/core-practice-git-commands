9de2cc79f4205dbcbcddd0355c0e244cd703f48c
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-22 15:47:10 -0400

    need to commit

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pytest
 
 
 def always_returns_true():
-<<<<<<< HEAD
-    x = 3 + 4
-    return x
-=======
-    return "True"
 
->>>>>>> 7aab387119de7593008ae3790958ab8c65ef49ff
+
+<< << << < HEAD
+x = 3 + 4
+return x
+== == == =
+return "False"
+
+>>>>>> > 7aab387119de7593008ae3790958ab8c65ef49ff
 
 
 def test_always_returns_true():
